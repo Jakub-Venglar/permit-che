03c77dff63357bfa474fc1702ec9efd663a44241
Jakub-Venglar <jakub.venglar@seznam.cz>
2023-02-08 15:58:30 +0100

    to do and initial proof

--- permit check.py
+++ permit check.py
@@ -1,1 +1,16 @@
 #! python3
+
+import requests, re, os, sys
+
+#make sure working directory is set the same as file directory
+os.chdir(os.path.dirname(sys.argv[0]))
+
+page = requests.get('https://portal.permit.pcta.org/availability/mexican-border.php', headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
+print(page.text)
+
+#make regex for getting only dict
+#eval dict
+#for loop for finding proper dict key
+
+# bezi kontinualne
+# posli email
