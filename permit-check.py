8972ca6c376c2c73cc155a402d2bdc3b4173ccd6
Jakub-Venglar <jakub.venglar@seznam.cz>
2023-02-09 23:27:48 +0100

    saving to file, date comparison

--- permit-check.py
+++ permit-check.py
@@ -5,20 +5,35 @@ import requests, re, time, getpass, time, os, json, datetime
 import smtplib, ssl
 from email.message import EmailMessage
 
-#nastav veci pro email
 
 port = 465  # For SSL
-#smtp_server = input("smtp server, např. smtp.seznam.cz:")
-smtp_server = 'smtp.seznam.cz'
-sender_email = input("Z jakého emailu to budeme posílat:")  
-receiver_email = input("A kam:") 
-password = getpass.getpass("Napiš heslo a zmáčkni enter: ")
-initial_date = input("Napiš první datum, které chceš prověřit. \nNapř. 2023-04-01 (dodrž formát)")
-end_date = input("Napiš poslední datum, které chceš prověřit. \nNapř. 2023-05-31 (dodrž formát), ale můžeš použít 2023-07-01 nebo vyšší pro testovací účely:")
+
+#nejdrive zkus nacist data ze souboru
+
+try:   
+    with open('data.json', 'r', encoding='utf-8') as file:
+        data =  json.loads(file.read())
+
+except FileNotFoundError:
+    print('Zeptám se tě na pár úvodních nastavení. Ta se uloží do souboru "data.json". Pokud bys je chtěl změnit, uprav je v soboru nebo ho smaž a spusť znovu program.')
+    smtp_server = input("smtp server, např. smtp.seznam.cz:")
+    sender_email = input("Z jakého emailu to budeme posílat:")  
+    receiver_email = input("A kam:") 
+    initial_date = input("Napiš první datum, které mám otestovat. \nNapříklad 2023-04-01 (dodrž formát):")
+    end_date = input("Napiš poslední datum, které mám otestovat. \nNapříklad 2023-05-31 (dodrž formát), ale můžeš použít 2023-07-01 nebo vyšší pro testovací účely:")
+
+    data = {'smtp_server':smtp_server,'sender_email':sender_email,'receiver_email':receiver_email,'initial_date':initial_date,'end_date':end_date}
+
+    with open('data.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent = 4) 
+
+
+password = getpass.getpass("Napiš heslo k ODESÍLACÍMU emailu a zmáčkni enter: ")
 
 #make sure working directory is set the same as file directory
 
-print(os.getcwd())
+
+
 
 # hlavni smycka
 
@@ -43,48 +58,50 @@ while True:
 
     for item in dateList:
 
-        if int(item['num']) < 50:
-            print('VOLNO')
-            print(item['start_date'])
+        if convert_date(item['start_date']) >= convert_date(data['initial_date']):
+
+            if int(item['num']) < 50:
+                print('VOLNO')
+                print(item['start_date'])
 
 
-            # sestav a odesli zpravu - musi byt v loop, pokud by bylo vice mist (jeste lepe - )
+                # sestav a odesli zpravu - musi byt v loop, pokud by bylo vice mist (jeste lepe - )
 
-            date = item['start_date']
-            spots = 50 - int(item['num'])
-            subject = f'Volné místo {date} na PCT'
+                date = item['start_date']
+                spots = 50 - int(item['num'])
+                subject = f'Volné místo {date} na PCT'
 
-            message = f"""Milý Hynečku
-            
-            Našel jsem pro tebe volné místo na tvé pouti.
+                message = f"""Milý Hynečku
+                
+                Našel jsem pro tebe volné místo na tvé pouti.
 
-            Toto místo je na den {date} a je jich právě {spots}.
+                Toto místo je na den {date} a je jich právě {spots}.
 
-            Tak rychle klikej, než ti ho vyfouknou.
+                Tak rychle klikej, než ti ho vyfouknou.
 
-            https://portal.permit.pcta.org/availability/mexican-border.php
+                https://portal.permit.pcta.org/availability/mexican-border.php
 
-            Tvůj věrný robotický otrok
-                    
-            """
+                Tvůj věrný robotický otrok
+                        
+                """
 
-            msg = EmailMessage()
-            msg.set_content(message)
-            msg['Subject'] = subject
-            msg['From'] = sender_email
-            msg['To'] = receiver_email
+                msg = EmailMessage()
+                msg.set_content(message)
+                msg['Subject'] = subject
+                msg['From'] = data['sender_email']
+                msg['To'] = data['receiver_email']
 
 
-            try:
-                context = ssl.create_default_context()
-                with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-                    server.login(sender_email, password)
-                    server.send_message(msg, from_addr=sender_email, to_addrs=receiver_email)
-                print('Email úspěšně odeslán')
+                try:
+                    context = ssl.create_default_context()
+                    with smtplib.SMTP_SSL(data['smtp_server'], port, context=context) as server:
+                        server.login(data['sender_email'], password)
+                        server.send_message(msg, from_addr=data['sender_email'], to_addrs=data['receiver_email'])
+                    print('Email úspěšně odeslán')
 
-            except Exception as e:
-                print('Něco se nepovedlo a email jsem neodeslal. Jsi připojen k internetu?')
-                print(e)
+                except Exception as e:
+                    print('Něco se nepovedlo a email jsem neodeslal. Jsi připojen k internetu?')
+                    print(e)
         
         if item['start_date'] == end_date:
             break # po kontrole tohoto data skoncime
