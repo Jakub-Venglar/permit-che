c15bc35dd7d12840e05d51750fa37041d8d60da0
Jakub-Venglar <jakub.venglar@seznam.cz>
2023-02-09 23:47:38 +0100

    try except on main loop so it won't fall

--- permit-check.py
+++ permit-check.py
@@ -42,69 +42,72 @@ def convert_date(string):
 # hlavni smycka
 
 while True:
+    try:
+        #stahni stranku
 
-    #stahni stranku
+        page = requests.get('https://portal.permit.pcta.org/availability/mexican-border.php', headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
 
-    page = requests.get('https://portal.permit.pcta.org/availability/mexican-border.php', headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
+        all = page.text
+        start = '{"limit":50,"calendar":'
+        end= '};var canadaStart'
 
-    all = page.text
-    start = '{"limit":50,"calendar":'
-    end= '};var canadaStart'
+        # vyber si relevantni cast, konvertuj do objektu a prober ho
 
-    # vyber si relevantni cast, konvertuj do objektu a prober ho
+        result = re.search(f'{start}(.*){end}', all)
+        dateList = eval(result.group(1))
 
-    result = re.search(f'{start}(.*){end}', all)
-    dateList = eval(result.group(1))
+        for item in dateList:
 
-    for item in dateList:
+            if convert_date(item['start_date']) >= convert_date(data['initial_date']):
 
-        if convert_date(item['start_date']) >= convert_date(data['initial_date']):
-
-            if int(item['num']) < 50:
-                print('VOLNO')
-                print(item['start_date'])
-
-
-                # sestav a odesli zpravu
-
-                date = item['start_date']
-                spots = 50 - int(item['num'])
-                subject = f'Volné místo {date} na PCT'
-
-                message = f"""Milý Hynečku
-                
-                Našel jsem pro tebe volné místo na tvé pouti.
-
-                Toto místo je na den {date} a je jich právě {spots}.
-
-                Tak rychle klikej, než ti ho vyfouknou.
-
-                https://portal.permit.pcta.org/availability/mexican-border.php
-
-                Tvůj věrný robotický otrok
-                        
-                """
-
-                msg = EmailMessage()
-                msg.set_content(message)
-                msg['Subject'] = subject
-                msg['From'] = data['sender_email']
-                msg['To'] = data['receiver_email']
+                if int(item['num']) < 50:
+                    print('VOLNO')
+                    print(item['start_date'])
 
 
-                try:
-                    context = ssl.create_default_context()
-                    with smtplib.SMTP_SSL(data['smtp_server'], port, context=context) as server:
-                        server.login(data['sender_email'], password)
-                        server.send_message(msg, from_addr=data['sender_email'], to_addrs=data['receiver_email'])
-                    print('Email úspěšně odeslán')
+                    # sestav a odesli zpravu
 
-                except Exception as e:
-                    print('Něco se nepovedlo a email jsem neodeslal. Jsi připojen k internetu?')
-                    print(e)
-        
-        if item['start_date'] == data['end_date']:
-            break # po kontrole tohoto data skoncime
+                    date = item['start_date']
+                    spots = 50 - int(item['num'])
+                    subject = f'Volné místo {date} na PCT'
+
+                    message = f"""Milý Hynečku
+                    
+                    Našel jsem pro tebe volné místo na tvé pouti.
+
+                    Toto místo je na den {date} a je jich právě {spots}.
+
+                    Tak rychle klikej, než ti ho vyfouknou.
+
+                    https://portal.permit.pcta.org/availability/mexican-border.php
+
+                    Tvůj věrný robotický otrok
+                            
+                    """
+
+                    msg = EmailMessage()
+                    msg.set_content(message)
+                    msg['Subject'] = subject
+                    msg['From'] = data['sender_email']
+                    msg['To'] = data['receiver_email']
+
+
+                    try:
+                        context = ssl.create_default_context()
+                        with smtplib.SMTP_SSL(data['smtp_server'], port, context=context) as server:
+                            server.login(data['sender_email'], password)
+                            server.send_message(msg, from_addr=data['sender_email'], to_addrs=data['receiver_email'])
+                        print('Email úspěšně odeslán')
+
+                    except Exception as e:
+                        print('Něco se nepovedlo a email jsem neodeslal. Jsi připojen k internetu?')
+                        print(e)
+            
+            if item['start_date'] == data['end_date']:
+                break # po kontrole tohoto data skoncime
+    except Exception as e:
+        print('Někde nastala chyba, ale script poběží dál znovu')
+        print(e)
 
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
