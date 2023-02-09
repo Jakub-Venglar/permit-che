d5a5eff290fbe0d2ef5db676d34615d9afe2d6d5
Jakub-Venglar <jakub.venglar@seznam.cz>
2023-02-09 21:53:59 +0100

    datetime add

--- permit-check.py
+++ permit-check.py
@@ -1,38 +1,45 @@
 #! python3
 
-import requests, re, time, getpass, time, os
+import requests, re, time, getpass, time, os, json, datetime
 
 import smtplib, ssl
 from email.message import EmailMessage
 
 #nastav veci pro email
 
-#port = 465  # For SSL
-##smtp_server = input("smtp server, např. smtp.seznam.cz:")
-#smtp_server = 'smtp.seznam.cz'
-#sender_email = input("Z jakého emailu to budeme posílat:")  
-#receiver_email = input("A kam:") 
-#password = getpass.getpass("Napiš heslo a zmáčkni enter: ")
-#end_date = input("Napiš datum od kterého už nebudu hledat. \nNejspíše 2023-05-31 (dodrž formát), ale můžeš použít 2023-07-01 nebo vyšší pro testovací účely:")
+port = 465  # For SSL
+#smtp_server = input("smtp server, např. smtp.seznam.cz:")
+smtp_server = 'smtp.seznam.cz'
+sender_email = input("Z jakého emailu to budeme posílat:")  
+receiver_email = input("A kam:") 
+password = getpass.getpass("Napiš heslo a zmáčkni enter: ")
+initial_date = input("Napiš první datum, které chceš prověřit. \nNapř. 2023-04-01 (dodrž formát)")
+end_date = input("Napiš poslední datum, které chceš prověřit. \nNapř. 2023-05-31 (dodrž formát), ale můžeš použít 2023-07-01 nebo vyšší pro testovací účely:")
 
 #make sure working directory is set the same as file directory
 
 print(os.getcwd())
 
-#stahni stranku
+# hlavni smycka
 
-page = requests.get('https://portal.permit.pcta.org/availability/mexican-border.php', headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
+def convert_date(self, string):
+    dateobj = datetime.datetime.strptime(string, '%Y-%m-%d')
 
-all = page.text
-start = '{"limit":50,"calendar":'
-end= '};var canadaStart'
-
-# vyber si relevantni cast, konvertuj do objektu a prober ho
-
-result = re.search(f'{start}(.*){end}', all)
-dateList = eval(result.group(1))
+    return dateobj
 
 while True:
+    #stahni stranku
+
+    page = requests.get('https://portal.permit.pcta.org/availability/mexican-border.php', headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
+
+    all = page.text
+    start = '{"limit":50,"calendar":'
+    end= '};var canadaStart'
+
+    # vyber si relevantni cast, konvertuj do objektu a prober ho
+
+    result = re.search(f'{start}(.*){end}', all)
+    dateList = eval(result.group(1))
 
     for item in dateList:
 
